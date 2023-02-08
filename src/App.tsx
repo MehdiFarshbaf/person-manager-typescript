26ea5705eadb5205774d2710bca20e8a11a6977e
Mehdi Farshbaf <mehdifarshbaf92@gmail.com>
2023-02-08 19:01:35 +0330

    init bootstrap and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         This is for test
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="">
+                    <p>this is for test</p>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
